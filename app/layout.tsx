57c7c876d2da32268b73c6731a73881617e346b4
Juan Ramirez <juanpram2000@gmail.com>
2024-09-25 23:52:45 -0700

    feat: start of front page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className="flex flex-col h-screen">
         <ThemeProvider themes={["nord", "night"]}>
           <Navbar />
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
 export default async function Page() {
-  return <main className="size-full"></main>;
+  return (
+    <main className="w-screen grow flex flex-col-reverse sm:flex-row items-center justify-around">
+      <section className="flex flex-col gap-2">
+        <h1 className="text-5xl font-semibold tracking-tighter">
+          Juan&apos;s Photography
+        </h1>
+
+        <p className="text-lg">Let me capture your moments</p>
+        <Link href={""} className="btn btn-primary">
+          Book Now
+        </Link>
+      </section>
+      <section className="w-1/2 h-full"></section>
+    </main>
+  );
 }
